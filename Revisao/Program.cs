25f40654a5e567f2f95ba13fe800396aa58850e3
DuduSeabra <duduseabra99@gmail.com>
2021-06-14 21:16:44 -0300

    definitive version

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,7 +6,84 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao = "1";
+            Aluno[] alunos = new Aluno[10];
+            int count = 0;
+
+            while(opcao != "0") 
+            {
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1- Cadastrar novo aluno");
+                Console.WriteLine("2- Calcular média geral");
+                Console.WriteLine("3- Listar alunos");
+                Console.WriteLine("0- Sair");
+                Console.WriteLine();
+
+                opcao = Console.ReadLine();
+
+                switch(opcao)
+                {
+                    case "1":
+                        Aluno aluno = new Aluno();
+
+                        Console.WriteLine("Informe o nome do aluno:");
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota:");
+                        if(decimal.TryParse(Console.ReadLine(), out decimal aux))
+                        {
+                            aluno.Nota = aux;
+                            alunos[count] = aluno;
+                            count++;
+                        }
+                        else 
+                            throw new ArgumentException("Valor da nota deve ser decimal.");
+                        
+                        break;
+
+                    case "2":
+                        decimal media = 0;
+                        int n_alunos = 0;
+
+                        for(int i = 0; i < alunos.Length; i++)
+                        {
+                            if(alunos[i].Nome != null)
+                            {
+                            media = alunos[i].Nota + media;
+                            n_alunos++;
+                            }
+                        }
+
+                        media = media/n_alunos;
+
+                        ConceitoEnum conceitogeral;
+
+                        if(media <= 2)
+                            conceitogeral = ConceitoEnum.E;
+                        else if(media <= 4)
+                            conceitogeral = ConceitoEnum.D;
+                        else if(media <= 6)
+                            conceitogeral = ConceitoEnum.C;
+                        else if(media <= 8)
+                            conceitogeral = ConceitoEnum.B;
+                        else
+                            conceitogeral= ConceitoEnum.A;
+
+                        Console.WriteLine($"A média entre os alunos é de {media} | O conceito geral entre os alunos é {conceitogeral}");
+                        break;
+                    case "3":
+                        foreach(Aluno i in alunos)
+                        {
+                            if(i.Nome != null)
+                                Console.WriteLine($"Nome: {i.Nome} | Nota: {i.Nota}");
+                        }
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        throw new ArgumentOutOfRangeException("Opção inválida, digite novamente.");
+                }
+            }            
         }
     }
 }
